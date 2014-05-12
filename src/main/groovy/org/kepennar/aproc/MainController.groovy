ce480b3b2e28adf77681f8b7593cea36af2c4a69
Kevin Pennarun <kevin.pennarun@gmail.com>
2014-05-13 01:00:46 +0200

    Add thymeleaf

--- src/main/groovy/org/kepennar/aproc/MainController.groovy
+++ src/main/groovy/org/kepennar/aproc/MainController.groovy
@@ -2,16 +2,25 @@ package org.kepennar.aproc;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/site")
 class MainController {
 
+	@RequestMapping(method = GET)
+	String getCurrentMenu(Model model) {
+			model.addAttribute("menuItems", null);
+			return "/home";
+		}
+	
     @RequestMapping(method=GET, value="{name}")
-    String home(@PathVariable String name) {
+    @ResponseBody String home(@PathVariable String name) {
 		return "Hello ${name}!"
     }
 
