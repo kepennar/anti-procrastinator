65961041321a35aea09bda5a12f14d3e83056f44
kepennar <kevin.pennarun@gmail.com>
2014-05-22 10:06:46 +0200

    Upgrade test config

--- src/cucumberTest/groovy/org/kepennar/aproc/RunCukes.groovy
+++ src/cucumberTest/groovy/org/kepennar/aproc/RunCukes.groovy
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber
 
 @RunWith(Cucumber)
 @cucumber.api.CucumberOptions(
-    format=["pretty", "html:build/reports/cucumber"],
+    format=["pretty", "html:build/reports/cucumberTest/cucumber"],
     strict=true,
     features=["src/cucumberTest/cucumber"],
     glue=["src/cucumberTest/groovy/org/kepennar/aproc/steps"],
