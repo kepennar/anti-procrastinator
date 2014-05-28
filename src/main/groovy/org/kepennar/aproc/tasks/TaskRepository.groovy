3541d77cb9eb65294ec8eeeff2f6f8720c3e8dc5
kepennar <kevin.pennarun@gmail.com>
2014-05-28 11:05:19 +0200

    Upgrade spring boot + Java 8 support

--- src/main/groovy/org/kepennar/aproc/tasks/TaskRepository.groovy
+++ src/main/groovy/org/kepennar/aproc/tasks/TaskRepository.groovy
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TaskRepository extends MongoRepository<Task, String> {
-	Task findByName(String name)
+public interface TaskRepository extends MongoRepository<Task, String> {
+	
+	Task findByName(String name);
 }
